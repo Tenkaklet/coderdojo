3d4eafa6bd65a2af71d924ae29036fbc61547e2c
Max Carlquist <tenkaklet@gmail.com>
2022-03-31 12:39:36 +0200

    changed capital letter

--- backend/app/libraries/pquery/constants.php
+++ backend/app/libraries/pquery/constants.php
@@ -1,7 +1,7 @@
 ***REMOVED***
 ***REMOVED***
-	define('AES_SECRET', 'ow9WxFe_4QEuQuy8Y8CMzhuzDSQHn66[');
-    define('DOMAIN', 'api.quicklytorsby.se');
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 
